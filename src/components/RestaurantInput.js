d7fe409116faf161ad9f44e6123e977fafee59a1
Erin Green <erin.y.green@gmail.com>
2019-11-07 15:16:25 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -49,6 +49,9 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapStateToProps = state => {
+  return { restaurants: state.restaurants }
+}
 
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(mapStateToProps)(RestaurantInput);
+
